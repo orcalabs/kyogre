44000f8bd566ce1cdb0968c5bcdd0306804488ad
Jon Foss Mikalsen <jon@orcalabs.no>
2023-01-06 07:55:15 +0100

    refactor: skip running migrations for now

--- src/database-migrator/src/main.rs
+++ src/database-migrator/src/main.rs
@@ -1,16 +1,14 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 
-use postgres::PostgresAdapter;
 use settings::Settings;
 
 pub mod settings;
 
 #[tokio::main]
 async fn main() {
-    let settings = Settings::new().unwrap();
+    let _settings = Settings::new().unwrap();
 
-    let adapter = PostgresAdapter::new(&settings.postgres).await.unwrap();
-
-    adapter.do_migrations().await;
+    // let adapter = PostgresAdapter::new(&settings.postgres).await.unwrap();
+    // adapter.do_migrations().await;
 }
