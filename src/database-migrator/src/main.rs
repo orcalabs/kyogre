5ec68fcba6dc05338c96287b3cce83ca7db8b58a
Jon Foss Mikalsen <jon@orcalabs.no>
2023-01-06 14:43:49 +0100

    refactor: re-enable database-migrator

--- src/database-migrator/src/main.rs
+++ src/database-migrator/src/main.rs
@@ -1,14 +1,15 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 
+use postgres::PostgresAdapter;
 use settings::Settings;
 
 pub mod settings;
 
 #[tokio::main]
 async fn main() {
-    let _settings = Settings::new().unwrap();
+    let settings = Settings::new().unwrap();
 
-    // let adapter = PostgresAdapter::new(&settings.postgres).await.unwrap();
-    // adapter.do_migrations().await;
+    let adapter = PostgresAdapter::new(&settings.postgres).await.unwrap();
+    adapter.do_migrations().await;
 }
