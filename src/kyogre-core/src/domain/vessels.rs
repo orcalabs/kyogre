2986d0eb464b394282e81bc5fc90ebb14d3e163f
joonnna <jonfossmikalsen1@gmail.com>
2023-02-21 08:48:45 +0100

    refactor: change sql names according to new fiskeridir names

--- src/kyogre-core/src/domain/vessels.rs
+++ src/kyogre-core/src/domain/vessels.rs
@@ -1,21 +1,5 @@
-use fiskeridir_rs::CallSign;
-
 #[derive(Debug, Clone)]
 pub struct Vessel {
     pub id: i64,
     pub mmsi: Option<i32>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct NewFiskeridirVessel {
-    pub id: i64,
-    pub call_sign: Option<CallSign>,
-    pub registration_id: String,
-    pub name: Option<String>,
-    pub hull_length: Option<f64>,
-    pub hull_width: Option<f64>,
-    pub building_year: Option<u32>,
-    pub engine_power: Option<u32>,
-    pub engine_building_year: Option<u32>,
-    pub owner: Option<String>,
 }
--- src/postgres/src/models/vessel.rs
+++ src/postgres/src/models/vessel.rs
@@ -34,33 +34,3 @@ impl TryFrom<AisVessel> for kyogre_core::AisVessel {
         })
     }
 }
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct FiskeridirVesselFromLanding {
-    // Fartøy ID
-    pub id: i64,
-    // Radiokallesignal (seddel)
-    pub call_sign: Option<CallSign>,
-    // Registreringsmerke (seddel)
-    pub registration_id: String,
-    // Fartøynavn
-    pub name: String,
-    // Største lengde
-    pub length: Option<f64>,
-    // Byggeår
-    pub building_year: Option<u32>,
-    // Motorkraft
-    pub engine_power: Option<u32>,
-    // Motorbyggeår
-    pub engine_building_year: Option<u32>,
-    // Fartøytype (kode)
-    pub vessel_type: i32,
-    // Fartøykommune (kode)
-    pub municipality_id: Option<i32>,
-    // Fartøyfylke (kode)
-    pub county_id: Option<i32>,
-    // Fartøynasjonalitet gruppe
-    pub nation_group_id: String,
-    // Fartøynasjonalitet (kode)
-    pub nation_id: String,
-}
