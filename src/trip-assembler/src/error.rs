f3a3c7a82469d90ac2487cf657a4b1782f1e5711
Markus Bergum Hilbertsen <markus@orcalabs.no>
2023-04-25 08:26:29 +0200

    fix: return error instead of unwrap

--- src/trip-assembler/src/error.rs
+++ src/trip-assembler/src/error.rs
@@ -1,4 +1,5 @@
 use error_stack::Context;
+use geoutils::Location;
 
 #[derive(Debug)]
 pub struct TripAssemblerError;
@@ -19,5 +20,22 @@ impl Context for TripPrecisionError {}
 impl std::fmt::Display for TripPrecisionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("an error occured during trip precision calculation")
+    }
+}
+
+#[derive(Debug)]
+pub struct LocationDistanceToError {
+    pub from: Location,
+    pub to: Location,
+}
+
+impl std::error::Error for LocationDistanceToError {}
+
+impl std::fmt::Display for LocationDistanceToError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "an error occured when calculating distance from {:?} to {:?}",
+            self.from, self.to
+        ))
     }
 }
