0f09750ec110194fc0f6f2e3197b5598d45ab66b
Markus Bergum Hilbertsen <markus@orcalabs.no>
2023-08-07 10:15:45 +0200

    fix: ais missing data threshold 60 -> 70

--- src/web-api/src/routes/v1/ais_vms.rs
+++ src/web-api/src/routes/v1/ais_vms.rs
@@ -15,7 +15,7 @@ use utoipa::{IntoParams, ToSchema};
 use super::ais::NavigationStatus;
 
 pub static AIS_VMS_DETAILS_INTERVAL: Lazy<Duration> = Lazy::new(|| Duration::minutes(30));
-pub static MISSING_DATA_DURATION: Lazy<Duration> = Lazy::new(|| Duration::minutes(60));
+pub static MISSING_DATA_DURATION: Lazy<Duration> = Lazy::new(|| Duration::minutes(70));
 
 #[derive(Debug, Deserialize, IntoParams)]
 #[serde(rename_all = "camelCase")]
