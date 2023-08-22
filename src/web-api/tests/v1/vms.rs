f38a4e9fd02bf148b4dbdc33e7fb4187a4f31c7f
joonnna <jonfossmikalsen1@gmail.com>
2023-08-22 12:56:15 +0200

    test: use test builder for vms tests

--- src/web-api/tests/v1/vms.rs
+++ src/web-api/tests/v1/vms.rs
@@ -59,22 +59,19 @@ async fn test_vms_return_bad_request_when_only_start_or_end_is_provided() {
 #[tokio::test]
 async fn test_vms_returns_the_last_24h_of_data_if_start_and_end_are_missing() {
     test(|helper| async move {
-        let cs = CallSign::try_from("TEST").unwrap();
-        let now = chrono::Utc::now();
-        let pos_inside_24h = helper.db.generate_vms_position(1, &cs, now).await;
-        let pos2_inside_24h = helper
-            .db
-            .generate_vms_position(2, &cs, now - Duration::seconds(1))
-            .await;
-        let _pos_outside_24h = helper
-            .db
-            .generate_vms_position(3, &cs, now - Duration::days(2))
+        let state = helper
+            .test_state_builder()
+            .position_start(Utc::now() - Duration::hours(26))
+            .position_increments(Duration::hours(3))
+            .vessels(1)
+            .vms_positions(3)
+            .build()
             .await;
 
         let response = helper
             .app
             .get_vms_positions(
-                &CallSign::try_from("TEST").unwrap(),
+                &state.vessels[0].fiskeridir.call_sign.clone().unwrap(),
                 VmsParameters {
                     start: None,
                     end: None,
@@ -83,7 +80,7 @@ async fn test_vms_returns_the_last_24h_of_data_if_start_and_end_are_missing() {
             .await;
         assert_eq!(response.status(), StatusCode::OK);
         let body: Vec<VmsPosition> = response.json().await.unwrap();
-        assert_eq!(vec![pos2_inside_24h, pos_inside_24h], body);
+        assert_eq!(state.vms_positions[1..], body);
     })
     .await;
 }
@@ -91,36 +88,26 @@ async fn test_vms_returns_the_last_24h_of_data_if_start_and_end_are_missing() {
 #[tokio::test]
 async fn test_vms_filters_by_start_and_end() {
     test(|helper| async move {
-        let cs = CallSign::try_from("TEST").unwrap();
-        let start = Utc.timestamp_opt(100, 0).unwrap();
-        let end = Utc.timestamp_opt(110, 0).unwrap();
-
-        helper
-            .db
-            .generate_vms_position(1, &cs, start - Duration::seconds(1))
-            .await;
-        let pos2 = helper
-            .db
-            .generate_vms_position(2, &cs, start + Duration::seconds(1))
-            .await;
-        helper
-            .db
-            .generate_vms_position(3, &cs, end + Duration::seconds(1))
+        let state = helper
+            .test_state_builder()
+            .vessels(1)
+            .vms_positions(3)
+            .build()
             .await;
 
         let response = helper
             .app
             .get_vms_positions(
-                &CallSign::try_from("TEST").unwrap(),
+                &state.vessels[0].fiskeridir.call_sign.clone().unwrap(),
                 VmsParameters {
-                    start: Some(start),
-                    end: Some(end),
+                    start: Some(state.vms_positions[0].timestamp + Duration::seconds(1)),
+                    end: Some(state.vms_positions[2].timestamp - Duration::seconds(1)),
                 },
             )
             .await;
         assert_eq!(response.status(), StatusCode::OK);
         let body: Vec<VmsPosition> = response.json().await.unwrap();
-        assert_eq!(vec![pos2], body);
+        assert_eq!(state.vms_positions[1..=1], body);
     })
     .await;
 }
