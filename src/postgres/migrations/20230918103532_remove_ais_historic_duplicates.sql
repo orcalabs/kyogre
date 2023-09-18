e7e38ec627754ff89e5e3801e86bce8486cd6960
joonnna <jonfossmikalsen1@gmail.com>
2023-09-18 09:57:08 +0200

    refactor: delete all duplicate historic ais messages

---
+++ src/postgres/migrations/20230918103532_remove_ais_historic_duplicates.sql
@@ -0,0 +1,13 @@
+DELETE FROM ais_vessels_historic a
+WHERE
+    TO_CHAR(a.mmsi, '9999999999999999') || TO_CHAR(a.message_timestamp, 'YYYY-MM-DD HH24:MM:SS') IN (
+        SELECT
+            TO_CHAR(b.mmsi, '9999999999999999') || TO_CHAR(b.message_timestamp, 'YYYY-MM-DD HH24:MM:SS')
+        FROM
+            ais_vessels_historic b
+        GROUP BY
+            b.mmsi,
+            b.message_timestamp
+        HAVING
+            COUNT(*) > 1
+    );
