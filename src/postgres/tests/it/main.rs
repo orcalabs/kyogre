b1de71ad6a1b5d0b60adeb65bcbfcc0b288d4e55
Markus Bergum Hilbertsen <markus@orcalabs.no>
2023-10-18 10:48:19 +0200

    fix: move test

--- src/postgres/tests/it/main.rs
+++
@@ -1,5 +0,0 @@
-#![deny(warnings)]
-#![deny(rust_2018_idioms)]
-
-mod helper;
-mod scraper_inbound_port;
--- src/web-api/tests/v1/landing.rs
+++ src/web-api/tests/v1/landing.rs
@@ -3,7 +3,7 @@ use actix_web::http::StatusCode;
 use chrono::{DateTime, Utc};
 use engine::*;
 use fiskeridir_rs::{GearGroup, SpeciesGroup};
-use kyogre_core::{LandingsSorting, Ordering};
+use kyogre_core::{FiskeridirVesselId, LandingsSorting, Ordering};
 use web_api::routes::{
     utils::{DateTimeUtc, GearGroupId, SpeciesGroupId},
     v1::landing::{Landing, LandingsParams},
@@ -265,6 +265,39 @@ async fn test_landings_sorts_by_weight() {
 
         assert_eq!(landings.len(), 4);
         assert_eq!(landings, state.landings);
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn test_landing_deletion_only_deletes_removed_landings() {
+    test(|helper, builder| async move {
+        let vessel_id = FiskeridirVesselId(1);
+        let landing = fiskeridir_rs::Landing::test_default(1, Some(vessel_id.0));
+        let landing2 = fiskeridir_rs::Landing::test_default(2, Some(vessel_id.0));
+        let landing3 = fiskeridir_rs::Landing::test_default(3, Some(vessel_id.0));
+        helper
+            .db
+            .add_landings(vec![landing.clone(), landing2.clone(), landing3.clone()])
+            .await;
+
+        let landings = helper.db.landing_ids_of_vessel(vessel_id).await;
+        assert_eq!(3, landings.len());
+        assert_eq!(landings[0], landing.id);
+        assert_eq!(landings[1], landing2.id);
+        assert_eq!(landings[2], landing3.id);
+
+        helper
+            .db
+            .add_landings(vec![landing.clone(), landing3.clone()])
+            .await;
+
+        let landings = helper.db.landing_ids_of_vessel(vessel_id).await;
+        assert_eq!(2, landings.len());
+        assert_eq!(landings[0], landing.id);
+        assert_eq!(landings[1], landing3.id);
+
+        builder.build().await;
     })
     .await;
 }
