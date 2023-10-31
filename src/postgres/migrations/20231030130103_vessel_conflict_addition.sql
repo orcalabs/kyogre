75f722aaee0955d3be5889c5aadbf8ab5ae245f9
joonnna <jonfossmikalsen1@gmail.com>
2023-10-31 08:42:31 +0100

    fix: handle existing vessels in migration

--- src/postgres/migrations/20231030130103_vessel_conflict_addition.sql
+++ src/postgres/migrations/20231030130103_vessel_conflict_addition.sql
@@ -2,13 +2,15 @@ INSERT INTO
     ais_vessels (mmsi)
 VALUES
     (258425000),
-    (257704000);
+    (257704000)
+ON CONFLICT DO NOTHING;
 
 INSERT INTO
     fiskeridir_vessels (fiskeridir_vessel_id)
 VALUES
     (2015071414),
-    (2021117417);
+    (2021117417)
+ON CONFLICT DO NOTHING;
 
 INSERT INTO
     fiskeridir_ais_vessel_mapping_whitelist (fiskeridir_vessel_id, call_sign, mmsi, is_manual)
