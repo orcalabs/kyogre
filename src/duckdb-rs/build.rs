fb9262af119867f506a985deb1116aba4407ac15
joonnna <jonfossmikalsen1@gmail.com>
2023-12-05 08:31:22 +0100

    build: add proto flag to support optional values

--- src/duckdb-rs/build.rs
+++ src/duckdb-rs/build.rs
@@ -1,4 +1,13 @@
+use std::path::PathBuf;
+use tonic_build::configure;
+
 fn main() {
-    tonic_build::compile_protos("proto/matrix_cache.proto")
+    let proto_path: PathBuf = "proto/matrix_cache.proto".into();
+    let proto_dir = proto_path
+        .parent()
+        .expect("proto file should reside in a directory");
+    configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["proto/matrix_cache.proto"], &[proto_dir])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
