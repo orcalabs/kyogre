83020fd1fcad87d2b1ea6565f1261dc017232b83
Markus Bergum Hilbertsen <markus@orcalabs.no>
2023-12-12 09:14:34 +0100

    fix: tests hanging

--- src/postgres/test_migrations/29991009075102_test_migration_remove_data.sql
+++ src/postgres/test_migrations/29991009075102_test_migration_remove_data.sql
@@ -25,6 +25,33 @@ WHERE
             (170901254)
     );
 
+UPDATE catch_locations
+SET
+    weather_location_ids = q.weather_location_ids
+FROM
+    (
+        SELECT
+            catch_location_id,
+            COALESCE(
+                ARRAY_AGG(weather_location_id) FILTER (
+                    WHERE
+                        weather_location_id IS NOT NULL
+                ),
+                '{}'
+            ) AS weather_location_ids
+        FROM
+            catch_locations c
+            INNER JOIN weather_locations w ON c."polygon" && w."polygon"
+        WHERE
+            w.weather_location_type_id = 1
+        GROUP BY
+            c.catch_location_id
+    ) q
+WHERE
+    q.catch_location_id = catch_locations.catch_location_id;
+
+DELETE FROM catch_location_daily_weather_dirty;
+
 DELETE FROM port_dock_points
 WHERE
     port_id NOT IN (
