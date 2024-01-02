3da4244ca67d9ede4363a0b2edbdabcb1c1e44c2
joonnna <jonfossmikalsen1@gmail.com>
2024-01-02 14:25:01 +0100

    test: add config tests

--- src/engine/tests/it/main.rs
+++ src/engine/tests/it/main.rs
@@ -1,6 +1,9 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 
+use config::{Config, File};
+use engine::Settings;
+
 pub mod ers_assembler;
 pub mod haul_distributor;
 pub mod helper;
@@ -8,3 +11,31 @@ pub mod landings_assembler;
 pub mod trip_distance;
 pub mod trips;
 pub mod trips_precision;
+
+#[test]
+fn test_local_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/local.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Local")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
+
+#[test]
+fn test_development_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/development.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Development")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
--- src/web-api/tests/main.rs
+++ src/web-api/tests/main.rs
@@ -1,4 +1,35 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 
+use config::{Config, File};
+use web_api::settings::Settings;
+
 pub mod v1;
+
+#[test]
+fn test_local_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/local.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Local")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
+
+#[test]
+fn test_development_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/development.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Development")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
