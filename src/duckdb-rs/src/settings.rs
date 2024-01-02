72309bfaca84e2b888dad1bab4715ba9f9ec9d55
joonnna <jonfossmikalsen1@gmail.com>
2024-01-02 14:44:02 +0100

    refactor: remove deprecated secret management

--- src/duckdb-rs/src/settings.rs
+++ src/duckdb-rs/src/settings.rs
@@ -1,4 +1,4 @@
-use config::{Config, ConfigError, File, Source};
+use config::{Config, ConfigError, File};
 use orca_core::{Environment, LogLevel, PsqlSettings, TelemetrySettings};
 use serde::Deserialize;
 
@@ -27,35 +27,15 @@ impl Settings {
             .try_into()
             .expect("failed to parse APP_ENVIRONMENT");
 
-        let mut builder = Config::builder()
+        Config::builder()
             .add_source(
                 File::with_name(&format!("config/{}", environment.as_str().to_lowercase()))
                     .required(false),
             )
             .add_source(config::Environment::with_prefix("KYOGRE_DUCKDB").separator("__"))
-            .set_override("environment", environment.as_str())?;
-
-        if environment == Environment::Development {
-            let database = config::File::with_name("/run/secrets/postgres-credentials.yaml")
-                .required(true)
-                .format(config::FileFormat::Yaml);
-            let map = database.collect()?;
-            builder = builder.set_override("postgres.ip", map["ip"].clone())?;
-            builder = builder.set_override("postgres.username", map["username"].clone())?;
-            builder = builder.set_override("postgres.password", map["password"].clone())?;
-
-            let honeycomb = config::File::with_name("/run/secrets/honeycomb-api-key")
-                .required(true)
-                .format(config::FileFormat::Yaml);
-
-            let map = honeycomb.collect()?;
-            builder = builder.set_override("honeycomb.api_key", map["api-key"].clone())?;
-        }
-        let config = builder.build()?;
-
-        let settings: Settings = config.try_deserialize()?;
-
-        Ok(settings)
+            .set_override("environment", environment.as_str())?
+            .build()?
+            .try_deserialize()
     }
 
     pub fn telemetry_endpoint(&self) -> Option<String> {
