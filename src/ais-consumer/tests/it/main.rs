d4d03fc9ac85d0ba467798d616212afba990c5c0
joonnna <jonfossmikalsen1@gmail.com>
2024-01-02 14:25:01 +0100

    test: add config tests

--- src/ais-consumer/tests/it/main.rs
+++ src/ais-consumer/tests/it/main.rs
@@ -1,5 +1,44 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 
+use ais_consumer::settings::Settings;
+use config::{Config, File};
+
 pub mod consumer;
 pub mod helper;
+
+#[test]
+fn test_local_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/local.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Local")
+        .unwrap()
+        .set_override("oauth.client_id", "test")
+        .unwrap()
+        .set_override("oauth.client_secret", "test")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
+
+#[test]
+fn test_development_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/development.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Development")
+        .unwrap()
+        .set_override("oauth.client_id", "test")
+        .unwrap()
+        .set_override("oauth.client_secret", "test")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
--- src/duckdb-rs/tests/it/main.rs
+++ src/duckdb-rs/tests/it/main.rs
@@ -1,2 +1,33 @@
+use config::{Config, File};
+use duckdb_rs::Settings;
+
 pub mod helper;
 pub mod refresh;
+
+#[test]
+fn test_local_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/local.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Local")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
+
+#[test]
+fn test_development_settings_are_valid() {
+    Config::builder()
+        .add_source(File::with_name("config/development.yml").required(true))
+        .set_override("postgres.username", "test")
+        .unwrap()
+        .set_override("environment", "Development")
+        .unwrap()
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+}
