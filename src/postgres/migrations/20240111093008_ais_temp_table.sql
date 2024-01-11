11be46226a3a6bcf593fed391c7f1fe8ab69a65f
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-01-11 10:31:58 +0100

    refactor: create ais temp table

---
+++ src/postgres/migrations/20240111093008_ais_temp_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ais_positions_temp AS TABLE ais_positions
+WITH
+    NO DATA;
+
+CREATE TABLE current_ais_positions_temp AS TABLE current_ais_positions
+WITH
+    NO DATA;
+
+CREATE TABLE ais_area_temp AS TABLE ais_area
+WITH
+    NO DATA;
