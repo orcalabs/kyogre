81236863276cc71c12f3b919f9c784f1fe2a03e0
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-01-15 09:49:01 +0100

    refactor: create new ais table

---
+++ src/postgres/migrations/20240111123315_ais_new_table.sql
@@ -0,0 +1,92 @@
+ALTER TABLE ais_positions
+RENAME TO ais_positions_old;
+
+CREATE TABLE ais_positions (
+    mmsi INT NOT NULL REFERENCES ais_vessels (mmsi),
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    course_over_ground DOUBLE PRECISION,
+    rate_of_turn DOUBLE PRECISION,
+    true_heading INT,
+    speed_over_ground DOUBLE PRECISION,
+    "timestamp" TIMESTAMPTZ NOT NULL,
+    altitude INT,
+    distance_to_shore DOUBLE PRECISION NOT NULL,
+    ais_class TEXT REFERENCES ais_classes (ais_class_id),
+    ais_message_type_id INT REFERENCES ais_message_types (ais_message_type_id),
+    navigation_status_id INT REFERENCES navigation_status (navigation_status_id),
+    PRIMARY KEY (mmsi, "timestamp")
+)
+PARTITION BY
+    LIST (mmsi);
+
+CREATE
+OR REPLACE FUNCTION add_ais_position_partition () RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            EXECUTE FORMAT(
+                $f$
+                    CREATE TABLE IF NOT EXISTS %I PARTITION OF ais_positions FOR VALUES IN (%L);
+                $f$,
+                CONCAT('ais_positions_', NEW.mmsi),
+                NEW.mmsi
+            );
+        END IF;
+
+        RETURN NEW;
+   END;
+$$;
+
+DO $$
+DECLARE
+    _r RECORD;
+BEGIN
+    FOR _r IN SELECT mmsi FROM ais_vessels
+    LOOP
+        EXECUTE FORMAT(
+            $f$
+                CREATE TABLE IF NOT EXISTS %I PARTITION OF ais_positions FOR VALUES IN (%L);
+            $f$,
+            CONCAT('ais_positions_', _r.mmsi),
+            _r.mmsi
+        );
+
+        INSERT INTO
+            ais_positions (
+                mmsi,
+                latitude,
+                longitude,
+                course_over_ground,
+                rate_of_turn,
+                true_heading,
+                speed_over_ground,
+                "timestamp",
+                altitude,
+                distance_to_shore,
+                ais_class,
+                ais_message_type_id,
+                navigation_status_id
+            )
+        SELECT
+            mmsi,
+            latitude::DOUBLE PRECISION,
+            longitude::DOUBLE PRECISION,
+            course_over_ground::DOUBLE PRECISION,
+            rate_of_turn::DOUBLE PRECISION,
+            true_heading,
+            speed_over_ground::DOUBLE PRECISION,
+            "timestamp",
+            altitude,
+            distance_to_shore::DOUBLE PRECISION,
+            ais_class,
+            ais_message_type_id,
+            navigation_status_id
+        FROM
+            ais_positions_old
+        WHERE
+            mmsi = _r.mmsi
+        ORDER BY
+            "timestamp";
+    END LOOP;
+END;
+$$;
