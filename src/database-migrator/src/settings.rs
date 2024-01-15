4d6e379dfe3b5d2e5e79e8ceb69272e570740e17
joonnna <jonfossmikalsen1@gmail.com>
2024-01-15 10:21:44 +0100

    build: build database-migrator

--- src/database-migrator/src/settings.rs
+++ src/database-migrator/src/settings.rs
@@ -1,4 +1,4 @@
-use config::{Config, File, Source};
+use config::{Config, File};
 use orca_core::{Environment, PsqlSettings};
 use serde::Deserialize;
 
@@ -14,23 +14,15 @@ impl Settings {
             .try_into()
             .expect("Failed to parse APP_ENVIRONMENT.");
 
-        let mut builder = Config::builder().add_source(
-            File::with_name(&format!("config/{}", environment.as_str().to_lowercase()))
-                .required(true),
-        );
-
-        if environment == Environment::Development {
-            let database = config::File::with_name("/run/secrets/postgres-credentials.yaml")
-                .required(true)
-                .format(config::FileFormat::Yaml);
-            let map = database.collect()?;
-            builder = builder.set_override("postgres.ip", map["ip"].clone())?;
-            builder = builder.set_override("postgres.username", map["username"].clone())?;
-            builder = builder.set_override("postgres.password", map["password"].clone())?;
-        }
-
-        let config = builder.build()?;
-
-        config.try_deserialize()
+        Config::builder()
+            .add_source(
+                File::with_name(&format!("config/{}", environment.as_str().to_lowercase()))
+                    .required(true),
+            )
+            .add_source(
+                config::Environment::with_prefix("KYOGRE_DATABASE_MIGRATOR").separator("__"),
+            )
+            .build()?
+            .try_deserialize()
     }
 }
