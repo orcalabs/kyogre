414109faaa179fa929218f83be210a719b3a3c56
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-09-12 08:45:42 +0200

    fix: don't insert after first push

--- src/scraper/src/chunks.rs
+++ src/scraper/src/chunks.rs
@@ -12,16 +12,15 @@ where
     A: Fn(Vec<D>) -> B,
     B: Future<Output = CoreResult<()>>,
 {
+    let modulo = chunk_size - 1;
     let mut chunk: Vec<D> = Vec::with_capacity(chunk_size);
 
     for (i, item) in data.enumerate() {
         match item {
-            Err(e) => {
-                error!("failed to read data: {e:?}");
-            }
+            Err(e) => error!("failed to read data: {e:?}"),
             Ok(item) => {
                 chunk.push(item);
-                if i % chunk_size == 0 {
+                if i % chunk_size == modulo {
                     insert_closure(chunk).await?;
                     chunk = Vec::with_capacity(chunk_size);
                 }
