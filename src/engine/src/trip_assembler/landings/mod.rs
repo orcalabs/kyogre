a69cdc76e1ad2fff9aa48829ce8dd314fbbb2e3d
joonnna <jonfossmikalsen1@gmail.com>
2024-10-07 15:30:48 +0200

    fix: ensure we use the latest trip to set trip calculation time

--- src/engine/src/trip_assembler/landings/mod.rs
+++ src/engine/src/trip_assembler/landings/mod.rs
@@ -130,7 +130,7 @@ async fn assemble_impl(
         Ok(None)
     } else {
         Ok(Some(TripAssemblerState {
-            calculation_timer: new_trips.last().unwrap().period.end(),
+            calculation_timer: new_trips.iter().map(|t| t.period.end()).max().unwrap(),
             new_trips,
             conflict_strategy: None,
         }))
