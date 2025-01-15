2f60923694dc99c772b080c3b75e7d15c70d7547
joonnna <jonfossmikalsen1@gmail.com>
2025-01-15 08:57:18 +0100

    refactor: filter out outliers in average eeoi

--- src/web-api/tests/v1/trip/benchmarks/eeoi.rs
+++ src/web-api/tests/v1/trip/benchmarks/eeoi.rs
@@ -157,3 +157,69 @@ async fn test_average_eeoi_works() {
     })
     .await;
 }
+
+#[tokio::test]
+async fn test_average_eeoi_works_on_single_trip() {
+    test(|helper, builder| async move {
+        let state = builder
+            .vessels(1)
+            .trips(1)
+            .landings(1)
+            .ais_positions(10)
+            .modify_idx(|i, v| {
+                v.position.latitude += i as f64;
+                v.position.longitude += i as f64;
+            })
+            .build()
+            .await;
+
+        let start = state.trips[0].period.start();
+        let end = state.trips.last().unwrap().period.end();
+
+        let eeoi = helper
+            .app
+            .get_average_eeoi(AverageEeoiParams {
+                start_date: start,
+                end_date: end,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        assert!(eeoi.unwrap() > 0.);
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn test_average_eeoi_works_on_two_trips() {
+    test(|helper, builder| async move {
+        let state = builder
+            .vessels(2)
+            .trips(2)
+            .landings(2)
+            .ais_positions(10)
+            .modify_idx(|i, v| {
+                v.position.latitude += i as f64;
+                v.position.longitude += i as f64;
+            })
+            .build()
+            .await;
+
+        let start = state.trips[0].period.start();
+        let end = state.trips.last().unwrap().period.end();
+
+        let eeoi = helper
+            .app
+            .get_average_eeoi(AverageEeoiParams {
+                start_date: start,
+                end_date: end,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        assert!(eeoi.unwrap() > 0.);
+    })
+    .await;
+}
