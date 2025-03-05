9da9538883f5efc8ca238fff9125a160162bc44e
Markus Bergum Hilbertsen <markus@orcalabs.no>
2025-03-05 10:15:34 +0100

    fix: mattilsynet csv changes

--- src/scraper/src/mattilsynet/mattilsynet_scraper.rs
+++ src/scraper/src/mattilsynet/mattilsynet_scraper.rs
@@ -1,24 +1,19 @@
 use std::collections::HashMap;
 
 use async_trait::async_trait;
-use fiskeridir_rs::DeliveryPointId;
-use http_client::{HttpClient, Response};
-use regex::Regex;
+use csv::Reader;
+use http_client::HttpClient;
 use table_extract::Table;
 use tracing::{error, info};
 
 use super::models::DeliveryPoint;
 use crate::{DataSource, Processor, Result, ScraperId, error::error::MissingValueSnafu};
 
-/// Statically know location for a file served by Mattilsynet.
-pub static MATTILSYNET_FILE_URL_PATH: &str =
-    "binary/Virksomheter_som_haandterer_fiskerivarer_-_Fishery_establishments.csv";
-
 pub struct MattilsynetScraper {
     http_client: HttpClient,
     approved_establishments_urls: Vec<String>,
-    fishery_establishments_url: Option<String>,
-    businesses_url: Option<String>,
+    fishery_products_url: Option<String>,
+    fishery_establishements_url: Option<String>,
 }
 
 #[async_trait]
@@ -44,26 +39,26 @@ impl DataSource for MattilsynetScraper {
 impl MattilsynetScraper {
     pub fn new(
         approved_establishments_urls: Option<Vec<String>>,
-        fishery_establishments_url: Option<String>,
-        businesses_url: Option<String>,
+        fishery_products_url: Option<String>,
+        fishery_establishements_url: Option<String>,
     ) -> Self {
         Self {
             http_client: Default::default(),
             approved_establishments_urls: approved_establishments_urls.unwrap_or_default(),
-            fishery_establishments_url,
-            businesses_url,
+            fishery_products_url,
+            fishery_establishements_url,
         }
     }
 
     async fn do_scrape(&self, processor: &(dyn Processor)) -> Result<()> {
         let approved = self.approved_establishments().await?;
-        let fishery = self.fishery_establishments().await?;
-        let businesses = self.businesses().await?;
+        let products = self.fishery_products().await?;
+        let establishments = self.fishery_establishments().await?;
 
         let mut delivery_points =
-            HashMap::with_capacity(approved.len() + fishery.len() + businesses.len());
+            HashMap::with_capacity(approved.len() + products.len() + establishments.len());
 
-        for d in approved.into_iter().chain(fishery).chain(businesses) {
+        for d in approved.into_iter().chain(products).chain(establishments) {
             delivery_points
                 .entry(d.id.clone())
                 .or_insert_with(|| d.into());
@@ -74,16 +69,14 @@ impl MattilsynetScraper {
             .await?)
     }
 
-    async fn get(&self, url: &str) -> Result<Response> {
-        Ok(self.http_client.get(url).send().await?)
+    async fn get(&self, url: &str) -> Result<String> {
+        Ok(self.http_client.get(url).send().await?.text().await?)
     }
 
     async fn approved_establishments(&self) -> Result<Vec<DeliveryPoint>> {
         let mut vec = Vec::new();
         for url in &self.approved_establishments_urls {
-            let response = self.get(url).await?;
-
-            let text = response.text().await?;
+            let text = self.get(url).await?;
 
             let table = Table::find_first(&text).ok_or_else(|| MissingValueSnafu.build())?;
 
@@ -97,11 +90,12 @@ impl MattilsynetScraper {
 
                 if let (Some(id), Some(name)) = (id, name) {
                     vec.push(DeliveryPoint {
-                        id: DeliveryPointId::try_from(id.clone())?,
+                        id: id.parse()?,
                         name: name.to_string(),
                         address: None,
                         postal_code: None,
                         postal_city: None,
+                        section: None,
                     });
                 }
             }
@@ -109,109 +103,37 @@ impl MattilsynetScraper {
         Ok(vec)
     }
 
-    async fn fishery_establishments(&self) -> Result<Vec<DeliveryPoint>> {
+    async fn fishery_products(&self) -> Result<Vec<DeliveryPoint>> {
         let mut vec = Vec::new();
-        if let Some(url) = &self.fishery_establishments_url {
-            let mut response = self.get(url).await?;
 
-            if !response.url().path().ends_with(MATTILSYNET_FILE_URL_PATH) {
-                response = self
-                    .get(&format!("{}/{}", response.url(), MATTILSYNET_FILE_URL_PATH))
-                    .await?;
-            }
+        if let Some(url) = &self.fishery_products_url {
+            let text = self.get(url).await?;
 
-            let text = response.text().await?;
+            let reader = Reader::from_reader(text.as_bytes());
 
-            for l in text.lines().skip(10) {
-                if l.starts_with(";;;") || l.starts_with(";Approval") || l.starts_with("Approval") {
-                    continue;
+            for v in reader.into_deserialize::<DeliveryPoint>() {
+                let v = v?;
+                if v.section
+                    .as_ref()
+                    .is_some_and(|v| v.starts_with("Section 8"))
+                {
+                    vec.push(v);
                 }
-
-                let (id_idx, name_idx, addr_idx, post_code_idx, post_city_idx) =
-                    if l.starts_with(';') {
-                        (1, 2, 3, 7, 8)
-                    } else {
-                        (0, 1, 2, 5, 6)
-                    };
-
-                let split = l.split(';').collect::<Vec<_>>();
-
-                let post_code_stripped = split[post_code_idx].replace(' ', "");
-                let postal_code: Option<u32> = if post_code_stripped.is_empty() {
-                    None
-                } else {
-                    Some(post_code_stripped.parse::<u32>()?)
-                };
-
-                let address = if split[addr_idx].is_empty() {
-                    None
-                } else {
-                    Some(split[addr_idx].to_string())
-                };
-
-                vec.push(DeliveryPoint {
-                    id: DeliveryPointId::try_from(split[id_idx].to_string())?,
-                    name: split[name_idx].to_string(),
-                    address,
-                    postal_code,
-                    postal_city: Some(split[post_city_idx].to_string()),
-                })
             }
         }
+
         Ok(vec)
     }
 
-    async fn businesses(&self) -> Result<Vec<DeliveryPoint>> {
-        let mut vec = Vec::new();
-        if let Some(url) = &self.businesses_url {
-            let text = self.get(url).await?.text().await?;
-
-            let address_code_city = Regex::new(r"(?i)^([^,]+)[, ]+(\d{4})\s+([^,]+)$")?;
-            let code_city = Regex::new(r"(?i)^(\d{4})\s+([^,]+)$")?;
-            let address_code = Regex::new(r"(?i)^([^,]+)[, ]+(\d{4})$")?;
-
-            for line in text.lines() {
-                let mut split = line.split(';');
-
-                let section = split.nth(5).ok_or_else(|| MissingValueSnafu.build())?;
-
-                if !section.contains("General activity establishment") {
-                    continue;
-                }
-
-                let id = split.next().ok_or_else(|| MissingValueSnafu.build())?;
-                let name = split.next().ok_or_else(|| MissingValueSnafu.build())?;
-                let address = split
-                    .next()
-                    .ok_or_else(|| MissingValueSnafu.build())?
-                    .trim();
-
-                let (address, postal_code, city) = if let Some((_, [addr, code, city])) =
-                    address_code_city.captures(address).map(|c| c.extract())
-                {
-                    (Some(addr), Some(code), Some(city))
-                } else if let Some((_, [code, city])) =
-                    code_city.captures(address).map(|c| c.extract())
-                {
-                    (None, Some(code), Some(city))
-                } else if let Some((_, [addr, code])) =
-                    address_code.captures(address).map(|c| c.extract())
-                {
-                    (Some(addr), Some(code), None)
-                } else {
-                    (None, None, None)
-                };
-
-                vec.push(DeliveryPoint {
-                    id: id.parse()?,
-                    name: name.trim().into(),
-                    address: address.map(|v| v.trim().into()),
-                    // unwrap is safe because regex only matches if postal_code is 4 digits
-                    postal_code: postal_code.map(|v| v.parse().unwrap()),
-                    postal_city: city.map(|v| v.trim().into()),
-                });
-            }
+    async fn fishery_establishments(&self) -> Result<Vec<DeliveryPoint>> {
+        if let Some(url) = &self.fishery_establishements_url {
+            let text = self.get(url).await?;
+            Reader::from_reader(text.as_bytes())
+                .into_deserialize::<DeliveryPoint>()
+                .collect::<csv::Result<_>>()
+                .map_err(|e| e.into())
+        } else {
+            Ok(vec![])
         }
-        Ok(vec)
     }
 }
--- src/scraper/src/mattilsynet/models.rs
+++ src/scraper/src/mattilsynet/models.rs
@@ -1,22 +1,26 @@
 use fiskeridir_rs::DeliveryPointId;
-use serde::Deserialize;
-use serde_with::{NoneAsEmptyString, serde_as};
+use serde::{
+    Deserialize, Deserializer,
+    de::{Error, Unexpected},
+};
 
-#[serde_as]
 #[derive(Debug, Clone, Deserialize)]
 pub(crate) struct DeliveryPoint {
-    #[serde(rename = "Approval number/ Godkjenningsnummer")]
+    #[serde(rename = "GODKJENNINGSNUMMER")]
     pub id: DeliveryPointId,
-    #[serde(rename = "Factory name")]
+    #[serde(rename = "VIRKSOMHETSNAVN", deserialize_with = "string_trim")]
     pub name: String,
-    #[serde(rename = "Address")]
-    #[serde_as(as = "NoneAsEmptyString")]
+    #[serde(rename = "ADRESSE", deserialize_with = "opt_string_trim")]
     pub address: Option<String>,
-    #[serde(rename = "Postal Code / Postnr")]
+    #[serde(rename = "POSTNR", deserialize_with = "opt_u32_trim")]
     pub postal_code: Option<u32>,
-    #[serde(rename = "City/ Poststed")]
-    #[serde_as(as = "NoneAsEmptyString")]
+    #[serde(rename = "POSTSTED", deserialize_with = "opt_string_trim")]
     pub postal_city: Option<String>,
+
+    // Only present for fishery products.
+    // Used for filtering out only `Section 8 - Fishery products`.
+    #[serde(rename = "SEKSJON", deserialize_with = "opt_string_trim", default)]
+    pub section: Option<String>,
 }
 
 impl From<DeliveryPoint> for kyogre_core::MattilsynetDeliveryPoint {
@@ -28,5 +32,39 @@ impl From<DeliveryPoint> for kyogre_core::MattilsynetDeliveryPoint {
             postal_city: v.postal_city,
             postal_code: v.postal_code,
         }
+    }
+}
+
+pub fn opt_string_trim<'de, D>(d: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let string = String::deserialize(d)?;
+    let trimmed = string.trim();
+
+    match trimmed.len() {
+        0 => Ok(None),
+        v if v == string.len() => Ok(Some(string)),
+        _ => Ok(Some(trimmed.into())),
+    }
+}
+
+pub fn string_trim<'de, D>(d: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    opt_string_trim(d)?.ok_or_else(|| {
+        Error::invalid_value(Unexpected::Other("empty string"), &"a non-empty string")
+    })
+}
+
+pub fn opt_u32_trim<'de, D>(d: D) -> Result<Option<u32>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(d)?;
+    match s.trim() {
+        "" => Ok(None),
+        v => Ok(v.parse().ok()),
     }
 }
