98df977a7e891d51188b846b06edd55484e3ed40
joonnna <jonfossmikalsen1@gmail.com>
2026-01-29 13:21:19 +0100

    fix: add new product-purpose

---
+++ src/postgres/migrations/20260129120653_add_product_purpose_hc_olje.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    product_purposes (product_purpose_id, name)
+VALUES
+    (832, 'HC Olje');
+
+INSERT INTO
+    landing_methods (landing_method_id, name)
+VALUES
+    (86, '15 kg emb. kartong');
