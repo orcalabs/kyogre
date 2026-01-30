9a783f7a6c55305e4cf7090c0349f4841087f6fd
joonnna <jonfossmikalsen1@gmail.com>
2026-01-30 08:36:55 +0100

    fix: add missing landing methods

---
+++ src/postgres/migrations/20260130072758_add_more_landing_methods.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    landing_methods (landing_method_id, name)
+VALUES
+    (87, '19 kg striesekk'),
+    (88, '16 kg kasterkartong'),
+    (89, 'embalert i sekk'),
+    (90, '14 kg emb. kartong');
